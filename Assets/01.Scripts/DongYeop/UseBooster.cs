333ab3d0eaa955bb4152eba482615cc887c81955
Dongyeop <kitty-777@naver.com>
2022-09-24 19:56:38 +0900

    부스터 제한, 부스터 잔량 슬라이더

--- Assets/01.Scripts/DongYeop/UseBooster.cs
+++ Assets/01.Scripts/DongYeop/UseBooster.cs
@@ -1,34 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UseBooster : MonoBehaviour
 {
     [SerializeField] private float _maxBooster = 100; // 사용할 수 있는 최대의 부스터량
 
     private Movement _movement;
+    private Slider _slider;
 
-    [SerializeField] private float _nowBooster; // 현제의 부스터 상황
     private float _booster = 5; // 초마다 사용하는 부스터의량
+    
+    public float nowBooster; // 현제의 부스터 상황
 
     private void Awake()
     {
         _movement = GetComponent<Movement>();
+        _slider = GameObject.Find("BoosterSlider").GetComponent<Slider>();
     }
 
     private void Start()
     {
-        _nowBooster = _maxBooster;
+        nowBooster = _maxBooster;
     }
 
     private void Update()
     {
         OnBooster();
+        SlideUpdate();
     }
 
     private void OnBooster()
     {
         if (_movement.isBooster)
-            _nowBooster -= _booster * Time.deltaTime;
+            nowBooster -= _booster * Time.deltaTime;
+    }
+
+    private void SlideUpdate()
+    {
+        _slider.value = nowBooster/_maxBooster;
     }
 }
